d2fb6a29fb16f02ccef31a98e5d8984063d0fde3
razakhalid <ra97za@gmail.com>
2024-05-01 16:06:44 -0500

    added styling and complete crud

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, NgZone} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {remult} from "remult";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
+
+export class AppModule {
+  constructor(zone: NgZone) {
+    remult.apiClient.wrapMessageHandling = (handler) => {
+      zone.run(() => handler());
+    }
+  }
+}
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,18 +2,50 @@ import { Component } from '@angular/core';
 import { Task } from "../../shared/task";
 import {remult} from "remult";
 import {NgFor} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  newTaskTitle:string = "";
+  async addTask() {
+    try {
+      const newTask = await this.taskRepo.insert({
+        title: this.newTaskTitle
+      });
+      this.tasks.push(newTask);
+      this.newTaskTitle = "";
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
+  async deleteTask(task: Task) {
+    try {
+      await this.taskRepo.delete(task);
+      this.tasks = this.tasks.filter(t => t !== task);
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
+  async saveTask(task: Task) {
+    try {
+      await this.taskRepo.save(task);
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
   tasks:Task[] = [];
   taskRepo = remult.repo(Task);
   ngOnInit() {
-    this.taskRepo.find().then(tasks => this.tasks = tasks);
+    this.taskRepo.find({
+      where: {
+        completed: undefined
+      }
+    }).then(tasks => this.tasks = tasks);
   }
 }
